03ef80c85272010f9f62ae945b7639f57083a004
User <User@HOME>
2023-12-10 17:11:13 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ToDoList
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.todoListBox = new System.Windows.Forms.ListBox();
+            this.todoTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // todoListBox
+            // 
+            this.todoListBox.BackColor = System.Drawing.Color.Gray;
+            this.todoListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.todoListBox.ForeColor = System.Drawing.Color.White;
+            this.todoListBox.FormattingEnabled = true;
+            this.todoListBox.ItemHeight = 25;
+            this.todoListBox.Location = new System.Drawing.Point(0, 0);
+            this.todoListBox.Name = "todoListBox";
+            this.todoListBox.Size = new System.Drawing.Size(542, 379);
+            this.todoListBox.TabIndex = 0;
+            this.todoListBox.SelectedIndexChanged += new System.EventHandler(this.todoListBox_SelectedIndexChanged);
+            // 
+            // todoTextBox
+            // 
+            this.todoTextBox.BackColor = System.Drawing.Color.Gray;
+            this.todoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.todoTextBox.ForeColor = System.Drawing.Color.White;
+            this.todoTextBox.Location = new System.Drawing.Point(548, 12);
+            this.todoTextBox.Name = "todoTextBox";
+            this.todoTextBox.Size = new System.Drawing.Size(390, 35);
+            this.todoTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.Silver;
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.addButton.Location = new System.Drawing.Point(548, 64);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(390, 50);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add a case";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.BackColor = System.Drawing.Color.Silver;
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.updateButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.updateButton.Location = new System.Drawing.Point(548, 130);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(390, 50);
+            this.updateButton.TabIndex = 3;
+            this.updateButton.Text = "Update the item";
+            this.updateButton.UseVisualStyleBackColor = false;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Silver;
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.deleteButton.Location = new System.Drawing.Point(548, 199);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(390, 50);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete selected";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // deleteAll
+            // 
+            this.deleteAll.BackColor = System.Drawing.Color.Silver;
+            this.deleteAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteAll.ForeColor = System.Drawing.Color.DarkGreen;
+            this.deleteAll.Location = new System.Drawing.Point(548, 264);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(390, 50);
+            this.deleteAll.TabIndex = 5;
+            this.deleteAll.Text = "Delete all";
+            this.deleteAll.UseVisualStyleBackColor = false;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Silver;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.ForeColor = System.Drawing.Color.Red;
+            this.exitButton.Location = new System.Drawing.Point(548, 329);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(390, 50);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(950, 384);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.deleteAll);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.todoTextBox);
+            this.Controls.Add(this.todoListBox);
+            this.Name = "MyForm";
+            this.Text = "To - do list";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox todoListBox;
+        private System.Windows.Forms.TextBox todoTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class MyForm : Form
+    {
+        
+        public MyForm()
+        {
+            InitializeComponent();
+
+            DateTime dateTime = DateTime.Now;
+            string dt = Convert.ToString(dateTime);
+
+            todoListBox.Items.Add($"1. Write a to - do list. {dt}");
+            todoListBox.Items.Add($"2. Pretend to be very busy with work. {dt}");
+            todoListBox.Items.Add($"3. Come up with an interesting point. {dt}");
+            todoListBox.Items.Add($"4. Surf the internet. {dt}");
+            todoListBox.Items.Add($"5. He said he was very tired. {dt}");
+            todoListBox.Items.Add($"6. Post a 'To - do list'. {dt}");            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Выход - использование диалогового окна
+            DialogResult result = MessageBox.Show(
+                "Do you really want to get out?",
+                "EXIT",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            string dt = Convert.ToString(dateTime);
+            if (todoTextBox.Text == String.Empty ||
+                todoTextBox.Text == null)
+            {
+                MessageBox.Show("Enter a value",
+                    "ERROR", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                todoListBox.Items.Add($"{todoListBox.Items.Count + 1}. " +
+                    $"{todoTextBox.Text} {dt}");
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            string dt = Convert.ToString(dateTime);
+            if (todoListBox.SelectedIndex != -1)
+            {
+                todoListBox.Items[todoListBox.SelectedIndex] = $"{todoListBox.SelectedIndex + 1}." +
+                    $" {todoTextBox.Text} {dt}";
+            }
+            else
+            {
+                MessageBox.Show("The objecthas not been selected for updating",
+                    "ERROR", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void todoListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            string dt = Convert.ToString(dateTime);
+            if (todoListBox.SelectedIndex != -1)
+            {
+                string selected = todoListBox.SelectedItem.ToString();
+                int l = selected.Length - dt.Length - selected.IndexOf(" ") - 1; 
+                todoTextBox.Text = selected.Substring(selected.IndexOf(" ") + 1, l);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (todoListBox.SelectedIndex != -1)
+            {
+                int selected = todoListBox.SelectedIndex;
+                todoListBox.Items.RemoveAt(selected);
+                for (int i = selected; i < todoListBox.Items.Count; i++)
+                {
+                    string strItem = todoListBox.Items[i].ToString();
+                    todoListBox.Items[i] = $"{i + 1}. {strItem.Substring(strItem.IndexOf(" ") + 1)}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nothing is selected",
+                    "ERROR  ",
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteAll_Click(object sender, EventArgs e)
+        {
+            DialogResult isOk = MessageBox.Show("Are you sure you want to delete it?",
+                "CLEARING", 
+                MessageBoxButtons.YesNo, 
+                MessageBoxIcon.Question);
+            if (isOk == DialogResult.Yes)
+            {
+                todoListBox.Items.Clear();
+            }
+        }
+    }
+}
